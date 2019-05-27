92f1de2bc17d878edb78ab4489f07583dcedb905
yangjianwei <2017585616@qq.com>
2019-05-27 16:50:08 +0800

    add dubbo-user

--- src/main/java/com/inspur/dubbo/App.java
+++ src/main/java/com/inspur/dubbo/App.java
@@ -1,13 +1,26 @@
 package com.inspur.dubbo;
 
+import com.inspur.dubbo.entity.OrderRequest;
+import com.inspur.dubbo.entity.OrderResponse;
+import com.inspur.dubbo.service.OrderService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:order-consumer.xml");
+
+        //模仿用户下单
+        OrderService orderService = (OrderService) context.getBean("orderService");
+
+        OrderRequest orderRequest = new OrderRequest();
+        orderRequest.setName("YangOrder");
+        OrderResponse orderResponse = orderService.doOrder(orderRequest);
+
+
+        //默认情况下,本地的Service 和
+        System.out.println(orderResponse);
     }
 }
