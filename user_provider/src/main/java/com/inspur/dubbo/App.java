89472fd72abd2fa59ec3717e56e39f05d81c3951
yangjianwei <2017585616@qq.com>
2019-05-29 10:33:21 +0800

    add dubbo-user

---
+++ user_provider/src/main/java/com/inspur/dubbo/App.java
@@ -0,0 +1,12 @@
+package com.inspur.dubbo;
+
+import com.alibaba.dubbo.container.Main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
